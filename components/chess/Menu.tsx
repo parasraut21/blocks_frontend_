5a84628127e990161495fa031205f48c53e8090e
Paras Raut <parasraut2511@gmail.com>
2023-09-06 17:11:36 +0530

    upadted

--- components/chess/Menu.tsx
+++ components/chess/Menu.tsx
@@ -10,8 +10,7 @@ import { addplayer1, addplayer2 } from "@/dapp/tezos";
 import SmokeBackground from "./Particles";
 import { useGame } from "@/contexts/GamesContext";
 import { useAccount } from "@/contexts/AccountContext";
-import { Button, Card,TextInput } from 'flowbite-react';
-
+import { Button, Card, TextInput } from "flowbite-react";
 
 export default function Menu() {
   const router = useRouter();
@@ -23,9 +22,7 @@ export default function Menu() {
   const [gameId, setGameId] = useState("");
   const { account } = useAccount();
 
-
-
-  const { makest , makerg , makejg } = useGame() || {};
+  const { makest, makerg, makejg } = useGame() || {};
 
   const handleGameIdChange = (event) => {
     setGameId(event.target.value);
@@ -38,15 +35,13 @@ export default function Menu() {
 
   const handleJoinWithGameId = async () => {
     makejg();
-    setLoading(true)
+    setLoading(true);
     console.log("Joining game with ID:", gameId);
 
-
-    
     await addplayer2(gameId).then((ans) => {
       if (ans) {
         router.push("/chess/" + gameId);
-        setLoading(false)
+        setLoading(false);
       }
     });
   };
@@ -79,14 +74,13 @@ export default function Menu() {
       });
     }
 
-
     socket.on("game id", gotogame);
     socket.on("ramdom id", gotoram);
     socket.emit("get-users");
     socket.on("get-users", (_users) => {
       setOnlineUsers(_users);
     });
-    
+
     return () => {
       socket.off("game id", gotogame);
       socket.off("get-users");
@@ -94,102 +88,116 @@ export default function Menu() {
   }, [socket]);
 
   const handlePrivateGameClick = () => {
-    makest()
+    makest();
     setLoading(true);
     socket?.emit("create");
-    
   };
-  const gotolib = () =>{
+  const gotolib = () => {
     router.push("/Library");
+  };
+
+  function BackgroundImage() {
+    return (
+      <div className="fixed top-0 left-0 w-full h-full z-[-1]">
+        <Image
+          src="/chess-bg-new.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="Chess Background"
+        />
+      </div>
+    );
   }
 
   return (
     <>
-     
-     <div className="bg-cover bg-no-repeat bg-center flex items-center justify-center w-screen h-screen md:ml-[-11rem]" style={{ backgroundImage: `url('/chess-bg.jpg')` }}>
-      <div className="relative h-full">
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="max-w-screen-md mx-auto">
-            <div className={styles["slow-moving-text"]}>
-              A chess game on the Tezos blockchain where you can stake 5 ꜩ to start either a private game or a public chess match at random. Win and receive 9 ꜩ, but if you leave early, the other player gets the prize. Blockchain incentives and strategic gameplay in one fascinating package.
-            </div>
-            <div className={styles["parti"]}>
-              <SmokeBackground />
+      <BackgroundImage />
+      <div className="relative flex items-center justify-center w-full h-screen bg-center bg-no-repeat bg-cover">
+        <div className="relative h-full">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="max-w-screen-md mx-auto">
+              <div className={styles["slow-moving-text"]}>
+                A chess game on the Tezos blockchain where you can stake 5 ꜩ to
+                start either a private game or a public chess match at random.
+                Win and receive 9 ꜩ, but if you leave early, the other player
+                gets the prize. Blockchain incentives and strategic gameplay in
+                one fascinating package.
+              </div>
+              <div className={styles["parti"]}>
+                <SmokeBackground />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center items-center h-full">
-          {loading ? (
-            <Loader />
-          ) : (
-            <Card className="max-w-sm bg-transparent border border-transparent border-opacity-0">
-               <Button 
-                      onClick={handlePrivateGameClick}
-                      className="menu-Button bg-customC hover:bg-customh transform hover:scale-105 text-black"
-                    >
-                      Private Game
-                    </Button>
-                    <Button
-                      onClick={() => {
-                        setLoading(true);
-                        socket?.emit("waitlist", username);
-                        makerg();
-                      }}
-                      className="menu-Button menu-Button-secondary bg-customC hover:bg-customh text-black transform hover:scale-105 text-black"
-                    >
-                      Random Opponent
-                    </Button>
-                    <Button
-                      onClick={handleJoinGameClick}
-                      className="menu-Button menu-Button-secondary bg-customC hover:bg-customh text-black transform hover:scale-105 text-black"
-                    >
-                      Join Game
-                    </Button>
-                    {joiningGame && (
-              <div className="menu-Buttons-1 p-8 rounded-lg shadow-md w-full max-w-sm bg-gray-100">
-        <TextInput
-  value={gameId}
-  onChange={handleGameIdChange}
-  color="info"
-  id="input-info"
-  placeholder="Enter Game ID"
-  className={`w-full p-2 border-0 focus:outline-none focus:ring focus:border-blue-300 shadow-white hover:shadow-lg`}
-  required
-/> 
+          <div className="flex justify-center items-center h-full">
+            {loading ? (
+              <Loader />
+            ) : (
+              <Card className="max-w-sm bg-transparent border border-transparent border-opacity-0">
+                <Button
+                  onClick={handlePrivateGameClick}
+                  className="menu-Button bg-customC hover:bg-customh transform hover:scale-105 text-black"
+                >
+                  Private Game
+                </Button>
+                <Button
+                  onClick={() => {
+                    setLoading(true);
+                    socket?.emit("waitlist", username);
+                    makerg();
+                  }}
+                  className="menu-Button menu-Button-secondary bg-customC hover:bg-customh text-black transform hover:scale-105 text-black"
+                >
+                  Random Opponent
+                </Button>
+                <Button
+                  onClick={handleJoinGameClick}
+                  className="menu-Button menu-Button-secondary bg-customC hover:bg-customh text-black transform hover:scale-105 text-black"
+                >
+                  Join Game
+                </Button>
+                {joiningGame && (
+                  <div className="menu-Buttons-1 p-8 rounded-lg shadow-md w-full max-w-sm bg-gray-100">
+                    <TextInput
+                      value={gameId}
+                      onChange={handleGameIdChange}
+                      color="info"
+                      id="input-info"
+                      placeholder="Enter Game ID"
+                      className={`w-full p-2 border-0 focus:outline-none focus:ring focus:border-blue-300 shadow-white hover:shadow-lg`}
+                      required
+                    />
 
-
-                        <Button
-                          onClick={handleJoinWithGameId}
-                          className="menu-Button mt-4 w-full p-2 transform hover:scale-105 text-black bg-customC hover:bg-customh text-black rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                        >
-                          Join with Game ID
-                        </Button>
-                        {loading && <Loader />}
-                      </div>
-                    )}
-                     {showOnline && (
-            <div className="text-white p-2 font-bold animate-pulse text-white-500">
-            Online: <span className="font-bold animate-pulse text-bg-customh-500">{onlineUsers.length}</span>
-          </div>
-                  )}
-
-                  <div>
                     <Button
-                      onClick={gotolib}
-                      className="menu-Button bg-customC text-black hover:bg-customh transform hover:scale-105 text-black"
+                      onClick={handleJoinWithGameId}
+                      className="menu-Button mt-4 w-full p-2 transform hover:scale-105 text-black bg-customC hover:bg-customh text-black rounded-md focus:outline-none focus:ring focus:border-blue-300"
                     >
-                      Library
+                      Join with Game ID
                     </Button>
+                    {loading && <Loader />}
                   </div>
-                    </Card>
-           
-          )}
-        </div>
-      
-      </div>
-    </div>
-  
+                )}
+                {showOnline && (
+                  <div className="text-white p-2 font-bold animate-pulse text-white-500">
+                    Online:{" "}
+                    <span className="font-bold animate-pulse text-bg-customh-500">
+                      {onlineUsers.length}
+                    </span>
+                  </div>
+                )}
 
+                <div>
+                  <Button
+                    onClick={gotolib}
+                    className="menu-Button bg-customC text-black hover:bg-customh transform hover:scale-105 text-black"
+                  >
+                    Library
+                  </Button>
+                </div>
+              </Card>
+            )}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
